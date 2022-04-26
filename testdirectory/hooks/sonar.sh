c6d1a51dc7a5f2408447a69e8cfb144fff33f25c
abhijeetabanave <mails2abhijeet@gmail.com>
2022-04-26 16:50:03 +0530

    Create sonar.sh

---
+++ testdirectory/hooks/sonar.sh
@@ -0,0 +1,6 @@
+	git init
+	git add .
+	git commit -m "first commit"
+	git branch -M myrepotest2
+	git remote add origin https://abhijeetabanave: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@github.com/abhijeetabanave/testme.git
+	git push -u origin myrepotest2  
