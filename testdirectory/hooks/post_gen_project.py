3a8ba4fea0ec5119dbfb62d7abfc945ccb66ddc8
abhijeetabanave <mails2abhijeet@gmail.com>
2022-04-26 16:36:34 +0530

    Update post_gen_project.py

--- testdirectory/hooks/post_gen_project.py
+++ testdirectory/hooks/post_gen_project.py
@@ -1,6 +1,6 @@
 import os
 
-from invoke import run
+import invoke
 
 base_dir = os.getcwd()
 project_name = '{{cookiecutter.project_name}}'
@@ -9,15 +9,15 @@ project_path = f'{base_dir}/{project_name}'
 repo = '{{cookiecutter.repo_name}}'
 
     
-run("git init");
+invoke.run("git init");
 command = "git commit --message {}".format(quote("initial commit"))
-run("git add --all")
-run(command)
-run("git remote add origin {}".format(repo))
-run(
+invoke.run("git add --all")
+invoke.run(command)
+invoke.run("git remote add origin {}".format(repo))
+invoke.run(
         "git remote add origin {}".format(
         quote("https://abhijeetabanave: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@github.com/abhijeetabanave/testme.git")
     )
 )
-run("git push -u origin {}".format(repo))
+invoke.run("git push -u origin {}".format(repo))
 
