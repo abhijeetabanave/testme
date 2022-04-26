0d3f982c56ea73026de9bca207f407175a5561f9
abhijeetabanave <mails2abhijeet@gmail.com>
2022-04-26 16:48:12 +0530

    Update post_gen_project.py

--- testdirectory/hooks/post_gen_project.py
+++ testdirectory/hooks/post_gen_project.py
@@ -1,6 +1,5 @@
 import os
-
-import invoke
+import subprocess
 
 base_dir = os.getcwd()
 project_name = '{{cookiecutter.project_name}}'
@@ -8,16 +7,10 @@ repo_dir = '{{cookiecutter.repo_dir}}'
 project_path = f'{base_dir}/{project_name}'
 repo = '{{cookiecutter.repo_name}}'
 
+
+print "start"
+subprocess.check_call("./script.sh %s %s %s" % (str(repo_dir), str(repo)), shell=True)
+print "end"
     
-invoke.run("git init");
-command = "git commit --message {}".format(quote("initial commit"))
-invoke.run("git add --all")
-invoke.run(command)
-invoke.run("git remote add origin {}".format(repo))
-invoke.run(
-        "git remote add origin {}".format(
-        quote("https://abhijeetabanave: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@github.com/abhijeetabanave/testme.git")
-    )
-)
-invoke.run("git push -u origin {}".format(repo))
+
 
