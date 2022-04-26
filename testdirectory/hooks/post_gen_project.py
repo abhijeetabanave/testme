e3fb013984e434c983ee471b2ffcb1fde9c466bc
abhijeet.abanave@citiustech.com <AbhijeetA2@citiustech.com>
2022-04-26 16:16:01 +0530

    add hook script

---
+++ testdirectory/hooks/post_gen_project.py
@@ -0,0 +1,22 @@
+import os
+from invoke import Result, run
+
+base_dir = os.getcwd()
+project_name = '{{cookiecutter.project_name}}'
+repo_dir = '{{cookiecutter.repo_dir}}'
+project_path = f'{base_dir}/{project_name}'
+repo = '{{cookiecutter.repo_name}}'
+
+    
+run("git init");
+command = "git commit --message {}".format(quote("initial commit"))
+run("git add --all")
+run(command)
+run("git remote add origin {}".format(repo))
+run(
+        "git remote add origin {}".format(
+        quote("https://abhijeetabanave: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@github.com/abhijeetabanave/testme.git")
+    )
+)
+run("git push -u origin {}".format(repo))
+
