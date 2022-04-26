4195600857f16a44b74f07fbb1e80f4eaf1494df
abhijeetabanave <mails2abhijeet@gmail.com>
2022-04-26 16:51:46 +0530

    Update post_gen_project.py

--- testdirectory/hooks/post_gen_project.py
+++ testdirectory/hooks/post_gen_project.py
@@ -7,7 +7,7 @@ repo_dir = '{{cookiecutter.repo_dir}}'
 project_path = f'{base_dir}/{project_name}'
 repo = '{{cookiecutter.repo_name}}'
 
-subprocess.check_call("./sonar.sh %s %s %s" % (str(repo_dir), str(repo)), shell=True)
+subprocess.check_call("./sonar.sh %s %s" % (str(repo_dir), str(repo)), shell=True)
 
 
 
